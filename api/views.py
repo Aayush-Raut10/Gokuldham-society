e353da60fc6ffe536d6356475afe19c9be2a485b
Aayush-Raut10 <aayushraut209@gmail.com>
2026-01-03 13:43:13 +0545

    add contact info to database

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from  api.models import memberDb
+from  api.models import memberDb, contactForm
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -20,22 +20,47 @@ def members_api(request):
         cleaned_data = json.loads(raw_data)
        
         print(cleaned_data["name"])
-        title = data["title"]
+        name = cleaned_data["name"]
        
-        if title:
+        # if name:
 
-            task = TaskDb.objects.create(task=title)
-            tasklist = {
-                "id": task.id,
-                "task": task.task
-                }
-            print(type(tasklist))
-            return JsonResponse(tasklist)
+        #     member = memberDb.objects.create(full_name=name)
+        #     members = {
+        #         "id": task.id,
+        #         "task": task.task
+        #         }
+            
+        #     return JsonResponse(members, )
 
-        return JsonResponse(10, safe=False)
+        # return JsonResponse(10, safe=False)
     
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)
 
+@csrf_exempt
+def contact_api(request):
+
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        name = data.get("name")
+        email = data.get("email")
+        message = data.get("message")
+
+        newContact = contactForm.objects.create(name=name, email=email,message=message)
+
+        print(newContact)
+        print(type(newContact))
+
+        response = {
+            'status':'success',
+            'message':'contacat saved successfully',
+            'saved_contact_info':{
+                'name':newContact.name,
+                'email':newContact.email,
+                'message':newContact.message
+            },
+        }
         
+        return JsonResponse(response)
 
